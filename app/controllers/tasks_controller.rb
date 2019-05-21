7759717b5ebb46f9aa6cc0c7a433d5c0d4212cf8
Darren Tonks <dtonks33@gmail.com>
2019-05-21 15:36:54 +0800

    Rails task manager code completed.  CSS needed.

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,37 @@
+class TasksController < ApplicationController
+  def show
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def detail
+    @task = Task.find(params[:id])
+  end
+
+  def create
+    @tasks = Task.new(title: params[:task][:title],
+                      details: params[:task][:details])
+    @tasks.save
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(title: params[:task][:title],
+                 details: params[:task][:details])
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+end
